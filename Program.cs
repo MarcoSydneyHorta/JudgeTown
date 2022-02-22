427ffa23277bad74c48d664aa86d65ac1a7c38e8
MarcoSydneyHorta <marco.sh@outlook.com>
2022-02-22 11:30:46 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace JudgeTown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] rasc = new int[] { 1, 3, 2, 3};
+            int d = 0;
+            int tam = rasc.Length / 2;
+            int[][] ent = new int[tam][];
+            ent[0] = new int[2];
+            ent[1] = new int[2];
+           // ent[2] = new int[2];
+            
+
+            for (int k = 0; k < tam; k++)
+            {
+                for (int l = 0; l < 2; l++)
+                {
+                    ent[k][l] = rasc[d];
+                    d++;
+                }
+            }
+
+            int res = Solution.findJudge(3, ent);
+            Console.WriteLine(" O juíz é o número " + res);
+            Console.Read();
+            /* Console.Write("Enter N: ");
+             var N = Console.ReadLine();
+             Console.Write("Enter input delimited by comma: ");
+             var input = Console.ReadLine();
+
+             var components = input.Split(',');
+             var myList = new List<int[]>();
+             var size = components.Length;
+
+             int[][] trust = new int[1][];
+             int[] arr = new int[size];
+
+             string[] result = new string[size];
+
+             for (int i = 0; i < size; i++)
+             {
+                 result = components[i].Split(' ');
+                 myList.Insert(i, new int[]{ int.Parse(result[0]), int.Parse(result[1]) });
+
+             }
+
+             trust = myList.Select(a => a.ToArray()).ToArray();
+             Console.WriteLine(Solution.findJudge(int.Parse(N), trust)); */
+
+            
+ 
+            Console.Read();
+
+        }
+    }
+
+    
+}
---
+++ Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace JudgeTown
+{
+    class Solution
+    {
+        public static int findJudge(int N, int[][] trust)
+        {
+            int ans = 0;
+            int tam = trust.Length;
+            int cit = 0;
+
+            int[] cada = new int[tam];
+
+            for (int i = 0; i < tam; i++)
+            {
+                cit = trust[i][1];
+            for (int j = 0; j < 2; j++)
+                {
+                if (trust[j][1] == cit)
+                    {
+                        cada[i]++;
+                    }
+
+                }
+
+            }
+
+            //ans = cada.Select(n => n).Max();
+            
+            for (int i = 0; i < tam; i++)
+            {
+                if ( cada[i] == N - 1)
+                {
+                    ans = trust[i][1];
+                }
+                else
+                {
+                    ans = -1;
+                }
+            }
+            return ans;
+        }
+    }
+}
